97359278d8dcf4301a9839cc29909cbcc5222fe2
Ajari Jules <ajarinjules@gmail.com>
2017-07-02 20:39:50 -0400

    using git

--- headlines.py
+++ headlines.py
@@ -1,32 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import feedparser
 
 app = Flask(__name__)
 
 RSS_FEED = {'bbc': "http://feeds.bbci.co.uk/news/rss.xml",
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
-            'Fox News': 'http://feeds.foxnews.com/foxnews/latest',
-            'IOL': 'http://rss.iol.io/iol/news'}
+            'fox': 'http://feeds.foxnews.com/foxnews/latest',
+            'iol': 'http://rss.iol.io/iol/news'}
 
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEED:
+        publication ='bbc'
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEED[publication])
-
-    html = ''
-
-    for article in feed['entries']:
-        html += """
-        <b>{0}</b> <br/>
-        <i>{1}</i> <br />
-        <p>{2}</p> <br/>""".format(article.get("title"), article.get("published"), article.get("summary"))
-    shell = """<html>
-    <body>
-        <h1> {0} Headlines </h1>
-    {1}
-    </body>
-    </html>""".format(publication.upper(), html)
     return render_template("home.html", articles = feed['entries'],publication=publication)
 
 
